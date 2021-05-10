cac033355056ffb168c7a8ad829ca43f85b394b9
Joeriubink <joeri.ubink@outlook.com>
2021-05-10 11:48:11 +0200

    Created Header + Footer
    - Created Header
    - Created Footer
    - Added images
    - Added some variables for breakpoints and color

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Footer.scss';
+
+class Footer extends React.Component {
+    render() {
+
+      return(
+          <footer className="footer">
+            <section className="footer__text">
+              <p>Text van footer</p>
+            </section>
+          </footer>
+      );
+    }
+}
+
+export default Footer;
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './index.scss';
 
-// import Header from "../../components/header/Header"
-// import Banner from "../../components/banner/Banner"
-// import Footer from "../../components/footer/Footer"
+import Header from "../../components/header/Header"
+import Footer from "../../components/footer/Footer"
 
 class IndexPage extends React.Component {
   render() {
     return(
       <section className="index">
+        <Header />
         <h1>DIT IS DE MAIN PAGE</h1>
+        <Footer />
       </section>
     );
   }
