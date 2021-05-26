131b887342e9f0d90486345faa6b11ef5b2aa79c
Kiwikisser <amos.peperkamp@gmail.com>
2021-05-26 13:20:58 +0200

    commented some code to prevent errors

--- src/pages/inventory/Inventory.jsx
+++ src/pages/inventory/Inventory.jsx
@@ -20,21 +20,21 @@ class Inventory extends React.Component {
     return r.keys().map(r);
   };
 
-  const images = this.importImages(require.context('../img/avatars', false, /\.(svg)/)); // Retrieve all images from '../img/avatars' and retrieve the webpack url
-
-    // Set all the avatar items based of the images in the folder and store them in 'imageList'
-    var imageList = images.map((image, index) =>{
-      return(
-        <label id={"avatar_label"+index} key={index}
-        onMouseDown={() => this.toggleHover(index)}
-        // onMouseLeave={this.toggleHover(index)}
-        className="modal-main--label"
-        >
-        <img className="modal-main--label--image " id={"avatar"+index} width="75px" src={image} style={hoverStyle} alt="Avatar"/>
-        <input className="modal-main--label--radio" onChange={this.handleSubmit} name="avatar_input" type="radio"></input>
-        </label>
-      );
-    });
+  // const images = this.importImages(require.context('../img/avatars', false, /\.(svg)/)); // Retrieve all images from '../img/avatars' and retrieve the webpack url
+  //
+  //   // Set all the avatar items based of the images in the folder and store them in 'imageList'
+  //   var imageList = images.map((image, index) =>{
+  //     return(
+  //       <label id={"avatar_label"+index} key={index}
+  //       onMouseDown={() => this.toggleHover(index)}
+  //       // onMouseLeave={this.toggleHover(index)}
+  //       className="modal-main--label"
+  //       >
+  //       <img className="modal-main--label--image " id={"avatar"+index} width="75px" src={image} style={hoverStyle} alt="Avatar"/>
+  //       <input className="modal-main--label--radio" onChange={this.handleSubmit} name="avatar_input" type="radio"></input>
+  //       </label>
+  //     );
+  //   });
 
 
   render(){
