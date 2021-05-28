dfd965509ae50ca16ae5adbafeb439a8b812f4b4
Kiwikisser <amos.peperkamp@gmail.com>
2021-05-28 16:36:34 +0200

    function to map material attributes into html elements

--- src/pages/inventory/Inventory.jsx
+++ src/pages/inventory/Inventory.jsx
@@ -1,47 +1,59 @@
 import React from "react";
 import './inventory.css';
+import axios from 'axios';
 
 import Header from "../../components/header/Header"
 import Footer from "../../components/footer/Footer"
+
+const MATERIALS_URL = 'http://localhost:8000/api/materials';
+// const MATERIALS_URL = 'http://localhost:8000/api/materials';
 
 class Inventory extends React.Component {
   constructor (){
     super();
 
     this.state = {
-
+      materials: [],
     };
-
-    this.importImages = this.importImages.bind(this);
   }
 
-  //This function is used to retrieve the location of certain files from a webpack directory given through 'r';
-  importImages = (r) =>{
-    return r.keys().map(r);
+  mapMaterials = () => {
+    console.log("materials function called");
+    for (let i = 0; i < this.state.materials.length; i++) {
+      this.state.materials[i].map(([name, description, amount, unit, added_by, img_url, ...rest]) => {
+        console.log(this.state.materials[i]);
+        return (
+         <>
+           <p> {name}</p>
+           <p> {description}</p>
+           <p> {amount}</p>
+           <p> {unit}</p>
+           <p> {added_by}</p>
+           <p> {img_url}</p>
+         </>
+        )
+      })
+    }
+  }
+
+  // This function gets called when the render() function gets called.
+  componentDidMount = () =>{
+    //Make the API GET request
+    axios.get(MATERIALS_URL)
+    .then((response) => {
+      //Retrieve the current set Avatar of the user in the DB and set it in state.
+      this.setState({materials: Object.values(response.data)});
+      console.log(this.state.materials[0]);
+    });
   };
-
-  // const images = this.importImages(require.context('../img/avatars', false, /\.(svg)/)); // Retrieve all images from '../img/avatars' and retrieve the webpack url
-  //
-  //   // Set all the avatar items based of the images in the folder and store them in 'imageList'
-  //   var imageList = images.map((image, index) =>{
-  //     return(
-  //       <label id={"avatar_label"+index} key={index}
-  //       onMouseDown={() => this.toggleHover(index)}
-  //       // onMouseLeave={this.toggleHover(index)}
-  //       className="modal-main--label"
-  //       >
-  //       <img className="modal-main--label--image " id={"avatar"+index} width="75px" src={image} style={hoverStyle} alt="Avatar"/>
-  //       <input className="modal-main--label--radio" onChange={this.handleSubmit} name="avatar_input" type="radio"></input>
-  //       </label>
-  //     );
-  //   });
-
 
   render(){
     return(
       <main>
         <Header />
-          <p>inventory</p>
+
+          { this.mapMaterials }
+
         <Footer />
       </main>
     );
