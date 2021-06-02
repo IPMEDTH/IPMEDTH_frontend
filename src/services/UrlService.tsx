ccb824dc6f4f504b5d26292bf80901c41931f9f7
Joeriubink <joeri.ubink@outlook.com>
2021-06-02 13:51:21 +0200

    Update UrlService.tsx
    fix CORS maybe?

--- src/services/UrlService.tsx
+++ src/services/UrlService.tsx
@@ -1,7 +1,7 @@
 let apiDomain = 'https://api.ipmedth.meulen.dev/'
 
 class UrlService {
-    static Devices() { return apiDomain + 'api/locations/'; }
+    static Devices() { return apiDomain + 'api/locations'; }
     static DeviceImages(filename) { return apiDomain + 'img/devices/' + filename; }
     static Materials() { return apiDomain + 'api/materials'}
     static Material(term) { return apiDomain + 'api/getmaterials/' + term; }
