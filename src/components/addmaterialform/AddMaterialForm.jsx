16b542888947418cfa996a4098cf8695296cab00
Kiwikisser <amos.peperkamp@gmail.com>
2021-06-07 16:59:33 +0200

    updated UrlService, removed input defaultvalue

--- src/components/addmaterialform/AddMaterialForm.jsx
+++ src/components/addmaterialform/AddMaterialForm.jsx
@@ -3,7 +3,7 @@ import UrlService from "../../services/UrlService";
 import axios from 'axios';
 import './addmaterialform.css';
 
-const COOKIE_URL = 'http://localhost:8000/sanctum/csrf-cookie';
+// const COOKIE_URL = 'http://localhost:8000/sanctum/csrf-cookie';
 
 class AddMaterialForm extends React.Component {
   constructor() {
@@ -25,9 +25,9 @@ class AddMaterialForm extends React.Component {
     const { name, description, amount, unit } = this.state;
 
     axios.defaults.withCredentials = true;
-    axios.get(COOKIE_URL)
+    axios.get(UrlService.getCookie())
     .then(response => {
-      axios.post('http://localhost:8000/api/postmaterial', { name, description, amount, unit })
+      axios.post(UrlService.PostMaterial(), { name, description, amount, unit })
         .then((result) => {
           console.log(result);
         })
@@ -42,16 +42,16 @@ class AddMaterialForm extends React.Component {
       <>
         <form className="addmaterial__form" method="POST">
           <label className="addmaterial__form__label" htmlFor="name">Naam
-            <input className="addmaterial__form__input" placeholder="placeholder" type="text" name="name"  defaultValue="default value" onChange={this.onChange} />
+            <input className="addmaterial__form__input" placeholder="placeholder" type="text" name="name"   onChange={this.onChange} />
           </label>
           <label className="addmaterial__form__label" htmlFor="description">Omschrijving
-            <input className="addmaterial__form__input" placeholder="placeholder" type="text" name="description"  defaultValue="default value" onChange={this.onChange} />
+            <input className="addmaterial__form__input" placeholder="placeholder" type="text" name="description"   onChange={this.onChange} />
           </label>
           <label className="addmaterial__form__label" htmlFor="amount">Hoeveelheid
             <input className="addmaterial__form__input" placeholder="placeholder" type="number" name="amount"  defaultValue="1" onChange={this.onChange} />
           </label>
           <label className="addmaterial__form__label" htmlFor="unit">Eenheid
-            <input className="addmaterial__form__input" placeholder="placeholder" type="text" name="unit"  defaultValue="default value" onChange={this.onChange} />
+            <input className="addmaterial__form__input" placeholder="placeholder" type="text" name="unit"   onChange={this.onChange} />
           </label>
           <button className="addmaterial__form__submit" type="submit" value="submit" onClick={this.submitForm}> Voeg Materiaal Toe &#10151;</button>
           <button className="addmaterial__form__close">Close</button>
--- src/services/UrlService.tsx
+++ src/services/UrlService.tsx
@@ -1,12 +1,13 @@
 let apiDomain = 'https://api.ipmedth.meulen.dev/'
-let apiLocal = 'http://localhost:8000/'
+// let apiLocal = 'http://localhost:8000/'
 
 class UrlService {
+    static getCookie() { return apiDomain + 'sanctum/csrf-cookie' }
     static Devices() { return apiDomain + 'api/locations/'; }
     static DeviceImages(filename) { return apiDomain + 'img/devices/' + filename; }
     static Materials() { return apiDomain + 'api/materials'}
     static Material(term) { return apiDomain + 'api/getmaterials/' + term; }
-    static PostMaterial() { return apiLocal + 'api/postmaterial' }
+    static PostMaterial() { return apiDomain + 'api/postmaterial' }
 }
 
 export default UrlService;
