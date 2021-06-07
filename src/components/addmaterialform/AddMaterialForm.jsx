4362e269d26c5112ef63c1faa505f86ae121be80
Kiwikisser <amos.peperkamp@gmail.com>
2021-06-07 20:49:02 +0200

    added addMaterialForm filled check

--- src/components/addmaterialform/AddMaterialForm.jsx
+++ src/components/addmaterialform/AddMaterialForm.jsx
@@ -16,26 +16,42 @@ class AddMaterialForm extends React.Component {
       location: '',
     };
   }
+
+  checkIfFormFilled = () => {
+    return (
+      this.state.name !== ''  &&
+      this.state.description !== ''  &&
+      this.state.amount !== ''  &&
+      this.state.unit !== ''  &&
+      this.state.location !== ''
+    )
+  }
+
   onChange = (e) =>{
     this.setState({ [e.target.name]: e.target.value });
+    console.log(e.target.value);
   }
 
   submitForm = (e) =>{
-
     e.preventDefault();
-    const { name, description, amount, unit, location } = this.state;
+    if (this.checkIfFormFilled()) {
+      const { name, description, amount, unit, location } = this.state;
 
-    axios.defaults.withCredentials = true;
-    axios.get(UrlService.getCookie())
-    .then(response => {
-      axios.post(UrlService.PostMaterial(), { name, description, amount, unit, location })
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    })
+      axios.defaults.withCredentials = true;
+      axios.get(UrlService.getCookie())
+      .then(response => {
+        axios.post(UrlService.PostMaterial(), { name, description, amount, unit, location })
+          .then((result) => {
+            console.log(result);
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      })
+    } else {
+      alert("Whoops! Je bent een veld vergeten in te vullen. \nVul a.u.b. voor verzenden alle velden in.");
+    }
+
   }
 
   render() {
@@ -45,19 +61,19 @@ class AddMaterialForm extends React.Component {
         <div className="addmaterial__titlebar"></div>
         <form className="addmaterial__form" method="POST">
           <label className="addmaterial__form__label" htmlFor="name">
-            <input className="addmaterial__form__input" placeholder="Naam" type="text" name="name"   onChange={this.onChange} />
+            <input className="addmaterial__form__input" placeholder="Naam" type="text" name="name"   onChange={this.onChange} required autoComplete='false'/>
           </label>
           <label className="addmaterial__form__label" htmlFor="description">
-            <input className="addmaterial__form__input" placeholder="Omschrijving" type="text" name="description"   onChange={this.onChange} />
+            <input className="addmaterial__form__input" placeholder="Omschrijving" type="text" name="description"   onChange={this.onChange} required autoComplete='false'/>
           </label>
           <label className="addmaterial__form__label" htmlFor="amount">
-            <input className="addmaterial__form__input" placeholder="Hoeveelheid" type="number" name="amount"  onChange={this.onChange} />
+            <input className="addmaterial__form__input" placeholder="Hoeveelheid" type="number" name="amount"  onChange={this.onChange} required autoComplete='false'/>
           </label>
           <label className="addmaterial__form__label" htmlFor="unit">
-            <input className="addmaterial__form__input" placeholder="Eenheid" type="text" name="unit"   onChange={this.onChange} />
+            <input className="addmaterial__form__input" placeholder="Eenheid" type="text" name="unit"   onChange={this.onChange} required autoComplete='false'/>
           </label>
           <label className="addmaterial__form__label" htmlFor="location">
-            <input className="addmaterial__form__input" placeholder="Waar ligt het?" type="text" name="location"   onChange={this.onChange} />
+            <input className="addmaterial__form__input" placeholder="Waar ligt het?" type="text" name="location"   onChange={this.onChange} required autoComplete='false'/>
           </label>
           <section className="addmaterial__form__buttons">
             <div className="addmaterial__form__buttons__close" onClick={this.props.closeModal}></div>
