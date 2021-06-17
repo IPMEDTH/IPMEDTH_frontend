9cd9c2315bf18efee09915ef6105405c30a9269c
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2021-06-17 15:53:30 +0200

    Login css tweaks

--- src/components/input_field/Input.jsx
+++ src/components/input_field/Input.jsx
@@ -4,10 +4,24 @@ import './input.scss';
 // Generic input field
 class Input extends React.Component {
   render() {
-    return <div className='Input'>
-              <label htmlFor={ this.props.name } ></label>
-              <input className="input__field" type={ this.props.type } name={ this.props.name } placeholder={ this.props.placeholder } value={this.props.value} onChange={this.props.onChange} required autoComplete='false'/>
-           </div>
+    return (
+      <div className='input'>
+        <label
+          className="input__label"
+          htmlFor={this.props.name}
+        >{this.props.label}</label>
+        <input
+          className="input__field"
+          type={ this.props.type }
+          name={ this.props.name }
+          placeholder={ this.props.placeholder }
+          value={this.props.value}
+          onChange={this.props.onChange}
+          required
+          autoComplete='false'
+        />
+      </div>
+    )
   }
 
 }
