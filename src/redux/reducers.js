54968a07b9bc24c842154262d058ff2185f4e76b
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2021-06-17 11:09:42 +0200

    Add redux

---
+++ src/redux/reducers.js
@@ -0,0 +1,28 @@
+import { CHANGE_ISLOGGEDIN, CHANGE_USER, CHANGE_LOCATIONS } from "./actions.js";
+
+export const isLoggedIn = (state = false, action) => {
+    switch (action.type) {
+        case CHANGE_ISLOGGEDIN:
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+export const user = (state = {}, action) => {
+    switch (action.type) {
+        case CHANGE_USER:
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+export const locations = (state = [], action) => {
+    switch (action.type) {
+        case CHANGE_LOCATIONS:
+            return action.payload;
+        default:
+            return state;
+    }
+};
