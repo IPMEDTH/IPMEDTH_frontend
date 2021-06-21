19a328590c99a377636eac20a704434f2452e61b
Joeriubink <joeri.ubink@outlook.com>
2021-06-21 14:16:51 +0200

    updated reservation title on myreservation

--- src/pages/my_reservations/Card.js
+++ src/pages/my_reservations/Card.js
@@ -11,7 +11,7 @@ class Card extends React.Component {
     var deviceid = this.props.id;
     axios.get(UrlService.DeviceMenu(deviceid), {}).then(res => {
       data = res.data.name;
-
+      document.getElementById("js--reservation__title").innerHTML = data;
       console.log(data);
     })
     .catch((error) => {
@@ -29,7 +29,7 @@ class Card extends React.Component {
         <article className="reservation__optie">
           <section className="reservation__optie__knop">
             <img className="reservation__optie__knop__image" src={this.props.img} alt={this.props.title} />
-            <p className="reservation__optie__knop__title">{data || "Apparaat"}</p>
+            <p id="js--reservation__title" className="reservation__optie__knop__title">{data || "Apparaat"}</p>
             <p className="reservation__optie__knop__description">Datum: {this.props.date || "Datum"}</p>
             <p className="reservation__optie__knop__starttime">Tijd: {this.props.starttime || "Tijd"} tot {this.props.endtime || "Tijd"}</p>
           </section>
