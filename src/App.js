5053e502a3b1e5b219de0b909a6bfa91e34877ff
Kiwikisser <amos.peperkamp@gmail.com>
2021-06-29 23:09:42 +0200

    reinserted my reminder pls don't remove thank you

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ const guestRoutes = [
   { path: "/material-history", component: None, exact: true },*/},
 ];
 
+console.log("w3 validated? :^)");
+
 class App extends Component {
   render() {
     return (
