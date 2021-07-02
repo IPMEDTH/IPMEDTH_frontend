f9917e968fdafa22f5c4333258048fde1fcbc085
Joeriubink <joeri.ubink@outlook.com>
2021-07-02 13:28:36 +0200

    bugfix changing starttime after endtime

--- src/pages/reservation/TimePicker1.js
+++ src/pages/reservation/TimePicker1.js
@@ -14,6 +14,10 @@ function TimePicker1() {
   const handleDateChange = (date) => {
     setSelectedDate(date);
     window.localStorage.setItem('timestart', date);
+
+    var timestart = window.localStorage.getItem('timestart');
+    timestart = timestart.substring(16,21);
+    window.localStorage.setItem('timestart', timestart);
   };
 
   return (
