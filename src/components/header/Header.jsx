aa72c077a51104db5e63bb50f44d19a9453601e1
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2021-07-05 16:22:30 +0200

    Add support for email validation to frontend

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { Icon } from '@material-ui/core';
 import logo from '../../images/logo.png';
@@ -7,6 +7,8 @@ import './Header.scss';
 import axios from "axios";
 import UrlService from "../../services/UrlService";
 import { changeUser } from "../../redux/actions";
+import { createBrowserHistory } from "history";
+import queryString from 'query-string';
 
 class Header extends React.Component {
 
@@ -22,12 +24,16 @@ class Header extends React.Component {
     axios.defaults.withCredentials = true;
     axios.get(UrlService.getUser())
     .then(response => {
-      console.log(response.data)
+      // console.log(response.data)
       this.props.dispatch(changeUser(response.data));
     })
     .catch(response => {
       console.log(response.error)
     });
+
+    if(queryString.parse(this.props.location.search).verified === '1') {
+      alert('E-mailvalidatie gelukt!')
+    }
   }
 
   render() {
@@ -42,8 +48,8 @@ class Header extends React.Component {
             <Link to="login" className={this.props.isLoggedIn ? 'header__hidden' : ''}>
               <Icon className="loginIcon">login</Icon>
             </Link>
-            <Link to="account" className={this.props.isLoggedIn ? '' : 'header__hidden'}>
-              <Icon className="loginIcon">account_circle</Icon>
+            <Link to={createBrowserHistory().location.pathname === "/account" ? 'editaccount' : 'account'} className={this.props.isLoggedIn ? '' : 'header__hidden'}>
+              <Icon className="loginIcon">{createBrowserHistory().location.pathname === "/account" ? 'edit' : 'account_circle'}</Icon>
             </Link>
 
         </header>
@@ -59,4 +65,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(withRouter(Header));
