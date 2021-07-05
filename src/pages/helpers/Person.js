5953e61b79d5a04b359516e165a7f332e2353d71
Joeriubink <joeri.ubink@outlook.com>
2021-07-05 19:52:45 +0200

    css helperpage

--- src/pages/helpers/Person.js
+++ src/pages/helpers/Person.js
@@ -7,7 +7,11 @@ class Person extends React.Component {
       <article className="helpers__card__container">
         <article className="helpers__optie">
           <section className="helpers__optie__knop">
-            <img className="helpers__optie__knop__image--person" src={this.props.img || "https://api.ipmedth.meulen.dev/persoon.png"} alt={this.props.title} />
+            <div className="helpers__optie__knop__imgdiv">
+              <figure className="helpers__optie__knop__imgfigure">
+                <img className="helpers__optie__knop__image--person" src={this.props.img || "https://api.ipmedth.meulen.dev/persoon.png"} alt={this.props.title} />
+              </figure>
+            </div>
             <p className="helpers__optie__knop__name"><b>{this.props.title || "Naam Persoon"}</b></p>
             <p className="helpers__optie__knop__knowledge"><b>Heeft kennis van:</b><br/>  {this.props.knowledge || "Onbekend"}</p>
             <p className="helpers__optie__knop__available"><b>Aanwezigheid:</b><br/> {this.props.available || "Onbekend"}</p>
