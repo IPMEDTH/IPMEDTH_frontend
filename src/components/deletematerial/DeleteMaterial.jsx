a6c9dcb0fc2d13098069be0678705d642ae0bc3e
Kiwikisser <amos.peperkamp@gmail.com>
2021-07-06 21:54:47 +0200

    added check to deleting material

--- src/components/deletematerial/DeleteMaterial.jsx
+++ src/components/deletematerial/DeleteMaterial.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import './deletematerial.scss';
 
 class DeleteMaterial extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      areYouSure: false,
+    };
+  }
+
+  deleteConfirmation = () => {
+    this.setState({areYouSure: true});
+  }
 
   render() {
     return(
       <section className="deletematerial">
-        <button className="deletematerial__button" onClick={this.props.onClick}/>
+      {this.state.areYouSure===false ?
+        <button className="deletematerial__button" onClick={this.deleteConfirmation}/>
+      :
+        <article className="deletematerial__areyousure">
+          <h3> Materiaal permanent verwijderen? </h3>
+          <button className="deletematerial__areyousure__yes" onClick={this.props.onClick}> Verwijder </button>
+        </article>
+      }
       </section>
     )
   }
