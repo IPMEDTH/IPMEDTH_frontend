1c45603dc3e6cc86b1d3f9aa64b5a6a22bb5c506
Kiwikisser <amos.peperkamp@gmail.com>
2021-07-07 12:02:48 +0200

    added navigation button for material history

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import {NavLink} from 'react-router-dom';
+import { connect } from "react-redux";
 import './index.scss';
 
 import Header from "../../components/header/Header"
 import Footer from "../../components/footer/Footer"
 
 class IndexPage extends React.Component {
+
+  checkUserRights = (user) => {
+    return user.isadmin!==undefined && user.isadmin!=="0";
+  }
+
   render() {
     return(
       <section className="index">
@@ -29,10 +35,13 @@ class IndexPage extends React.Component {
                 <NavLink exact className="index__content__selection-item" to="/my_reservations">
                   <h3 className="index__content__selection-item__title"> Jouw Reserveringen </h3>
                 </NavLink>
-                {/*
-                <NavLink exact className="index__content__selection-item" to="/materialhistory">
-                  <h3 className="index__content__selection-item__title"> Materiaal History </h3>
-                </NavLink> */}
+                {this.checkUserRights(this.props.user)===false ?
+                  null
+                :
+                  <NavLink exact className="index__content__selection-item" to="/materialhistory">
+                    <h3 className="index__content__selection-item__title"> Materiaal History </h3>
+                  </NavLink>
+                }
             </article>
           </div>
         </main>
@@ -43,4 +52,10 @@ class IndexPage extends React.Component {
   }
 }
 
-export default IndexPage;
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(IndexPage);
